eaf62f4d987338967cc55c4db0b8f36b94ee10ab
ProjektCity <projektcityofficial@gmail.com>
2024-11-26 20:20:07 +0100

    mainpage_downloadcount_js.js

--- mainpage_downloadcount_js.js
+++ mainpage_downloadcount_js.js
@@ -1,6 +1,6 @@
-const progressText = document.getElementById('progressText').innerText;
-const [currentValue, maxValue] = progressText.split(' / ').map(num => parseInt(num.replace(/,/g, ''), 10));
-const progressBar = document.getElementById('progressBar');
-const percentage = (currentValue / maxValue) * 100;
+        const progressText = document.getElementById('progressText').innerText;
+        const [currentValue, maxValue] = progressText.split(' / ').map(num => parseInt(num.replace(/,/g, ''), 10));
+        const progressBar = document.getElementById('progressBar');
 
-progressBar.style.width = percentage + '%';
+        const percentage = (currentValue / maxValue) * 100;
+        progressBar.style.width = percentage + '%';
